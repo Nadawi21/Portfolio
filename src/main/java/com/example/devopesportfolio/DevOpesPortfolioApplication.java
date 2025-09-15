c464a52630f2690c01ad0f31995f07d0e68b455c
LEB-Atheel <ado@lebsystem.se>
2025-09-15 22:00:43 +0200

    test

---
+++ src/main/java/com/example/devopesportfolio/DevOpesPortfolioApplication.java
@@ -0,0 +1,14 @@
+package com.example.devopesportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpesPortfolioApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpesPortfolioApplication.class, args);
+    }
+
+}
